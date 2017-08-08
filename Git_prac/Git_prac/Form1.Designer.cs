92243c933ad5c15398b9b02bb48f41fc1c1e0995
Jean <Jeanseemail@gmail.com>
2017-08-09 00:56:06 +0200

    initial project

---
+++ Git_prac/Git_prac/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Git_prac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnJean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnJean
+            // 
+            this.btnJean.Location = new System.Drawing.Point(33, 32);
+            this.btnJean.Name = "btnJean";
+            this.btnJean.Size = new System.Drawing.Size(75, 23);
+            this.btnJean.TabIndex = 0;
+            this.btnJean.Text = "Jéan";
+            this.btnJean.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 338);
+            this.Controls.Add(this.btnJean);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJean;
+    }
+}
+
