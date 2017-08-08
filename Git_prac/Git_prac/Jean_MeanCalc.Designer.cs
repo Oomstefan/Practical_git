cf3c5495bc3aafaa0eb63582184fd8114e9642ba
Jean <Jeanseemail@gmail.com>
2017-08-09 01:16:19 +0200

    Class created

--- Git_prac/Git_prac/Jean_MeanCalc.Designer.cs
+++ Git_prac/Git_prac/Jean_MeanCalc.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Jean_MeanCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 307);
+            this.Name = "Jean_MeanCalc";
             this.Text = "Jean_MeanCalc";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
