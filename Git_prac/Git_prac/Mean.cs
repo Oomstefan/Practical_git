27a8c70b2be07a9866b26a6a4f58691a6242f8af
Jean <Jeanseemail@gmail.com>
2017-08-09 01:57:23 +0200

    first feature commit

---
+++ Git_prac/Git_prac/Mean.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_prac
+{
+    class Mean
+    {
+        public int[] numbers;
+
+        public void Rand(int x)  {
+            numbers = new int[x];
+            Random rand = new Random();
+            for(int k = 0; k <= x; k++) {
+                numbers[k] = rand.Next();
+            }
+            
+        }
+
+        public int mean(int x) {
+            int sum = 0;
+            for(int l = 0; l < x; l++) {
+                sum += numbers[l];
+
+            }
+
+            return (sum / x);
+        
+
+
+        }
+
+
+
+    }
+}
