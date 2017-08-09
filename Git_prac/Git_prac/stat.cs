a629d92e59bd338b00590e5da82f124e1a8cdeed
Jean <Jeanseemail@gmail.com>
2017-08-09 02:29:50 +0200

    Moved mean to class stat

---
+++ Git_prac/Git_prac/stat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_prac
+{
+    class stat
+    {
+        //Jean mean func begin
+        //******************
+        public int[] numbers;
+        public string nrhold;
+
+        public void Rand(int x)
+        {
+            numbers = new int[x];
+            Random rand = new Random();
+            for (int k = 0; k < x; k++)
+            {
+                numbers[k] = rand.Next(1, 50);
+                nrhold += Convert.ToString(numbers[k]) + " ";
+            }
+
+        }
+
+        public float mean(int x)
+        {
+            int sum = 0;
+            for (int l = 0; l < x; l++)
+            {
+                sum += numbers[l];
+
+            }
+
+            return (sum / x);
+
+
+
+        }
+        //******************
+        //Jean mean func end
+
+
+
+    }
+}
